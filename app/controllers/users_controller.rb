178f59347061d7b1eb3c69cb35542947eb0e382c
rsinclairchin <r.sinclairchin@gmail.com>
2017-05-06 11:25:00 -0700

    Fills in user controller to create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+#shows the signup form
+  def new
+    render "new"
+  end
+
+#creates a new user
+  def create
+    User.create(user_params)
+  end
+
+private
+
+# retrieves user params from signup form
+  def user_params
+    params.permit(:name)
+  end
+
 end
